1948e0b6bc17bc4038036a7ea95a137edaf04efd
ungheung <t01047377477@gmail.com>
2024-11-12 22:25:50 +0900

    feat : 유저생성, 유저 정보 반환, 유저 존재여부 반환 로직 추가

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,46 @@
 import { Injectable } from '@nestjs/common';
+import { SignUpDto } from 'src/auth/dto/signUp.dto';
+import { Repository } from 'typeorm';
+import { User } from './entity/user.entity';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private readonly userRepository: Repository<User>) {}
+
+  async getUserById(id: number) {
+    const user = await this.userRepository
+      .createQueryBuilder('user')
+      .where('user.id = :id', { id })
+      .select(['user.id', 'user.password'])
+      .getOne();
+
+    return user;
+  }
+
+  async getUserByAccount(account: string) {
+    const user = await this.userRepository
+      .createQueryBuilder('user')
+      .where('user.account = :account', { account })
+      .select(['user.id', 'user.name', 'user.account', 'user.password'])
+      .getOne();
+
+    return user;
+  }
+
+  async createUser(dto: SignUpDto) {
+    const user = this.userRepository.create({
+      ...dto,
+    });
+
+    return user;
+  }
+
+  //
+  async existsUser(id: number) {
+    const isExists = await this.userRepository.exists({
+      where: { id },
+    });
+
+    return isExists;
+  }
+}
