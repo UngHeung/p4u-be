e8c2a4842bebd204bafffc7cc73efae5994cdd8d
ungheung <t01047377477@gmail.com>
2024-11-12 14:11:25 +0900

    update : entity 설정 추가

--- src/user/entity/user.entity.ts
+++ src/user/entity/user.entity.ts
@@ -2,7 +2,13 @@ import { Length, Matches } from 'class-validator';
 import { BaseModel } from 'src/common/entity/base.entity';
 import { Column, Entity } from 'typeorm';
 
-@Entity()
+@Entity({
+  name: 'users',
+  orderBy: {
+    id: 'DESC',
+    name: 'ASC',
+  },
+})
 export class User extends BaseModel {
   @Column({ nullable: false })
   @Length(2, 12, { message: '이름은 2자 이상 12자 이하로 입력해주세요.' })
@@ -17,6 +23,7 @@ export class User extends BaseModel {
     message: '아이디는 영문 소문자, 숫자로만 입력해주세요.',
   })
   account: string;
+
   @Column({ nullable: false })
   @Length(8, 16, { message: '비밀번호는 8자 이상 16자 이하로 입력해주세요.' })
   @Matches(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#$%]).{8,}$/g, {
