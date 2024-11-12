6bebf8c0b98312d958846d965572de7697e65fa3
ungheung <t01047377477@gmail.com>
2024-11-12 22:24:59 +0900

    feat : 로그인, 회원가입 api 추가

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Headers, Post } from '@nestjs/common';
+import { User } from 'src/user/entity/user.entity';
 import { AuthService } from './auth.service';
+import { SignUpDto } from './dto/signUp.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('signup')
+  registerUser(@Body() dto: SignUpDto): Promise<User> {
+    return this.authService.registerUser(dto);
+  }
+
+  @Post('signin')
+  loginUser(
+    @Headers('authorization') rawToken: string,
+  ): Promise<{ accessToken: string; refreshToken: string }> {
+    const token = this.authService.extractToken(rawToken, false);
+    const credentials = this.authService.decodeBasicToken(token);
+
+    return this.authService.login(credentials);
+  }
 }
