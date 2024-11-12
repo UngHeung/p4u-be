19732d100235ffc0d33099004dfe7a87f6d2b31c
ungheung <t01047377477@gmail.com>
2024-11-12 14:07:05 +0900

    feat : 유저 entity 추가

---
+++ src/user/entity/user.entity.ts
@@ -0,0 +1,27 @@
+import { Length, Matches } from 'class-validator';
+import { BaseModel } from 'src/common/entity/base.entity';
+import { Column, Entity } from 'typeorm';
+
+@Entity()
+export class User extends BaseModel {
+  @Column({ nullable: false })
+  @Length(2, 12, { message: '이름은 2자 이상 12자 이하로 입력해주세요.' })
+  @Matches(/[a-zA-Z가-힣]/g, {
+    message: '이름은 한글, 영문 대소문자로만 입력해주세요.',
+  })
+  name: string;
+
+  @Column({ nullable: false })
+  @Length(6, 12, { message: '아이디는 6자 이상 12자 이하로 입력해주세요.' })
+  @Matches(/[a-z0-9]/g, {
+    message: '아이디는 영문 소문자, 숫자로만 입력해주세요.',
+  })
+  account: string;
+  @Column({ nullable: false })
+  @Length(8, 16, { message: '비밀번호는 8자 이상 16자 이하로 입력해주세요.' })
+  @Matches(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#$%]).{8,}$/g, {
+    message:
+      '비밀번호는 영문 대소문자, 특수문자를 하나씩 포함하여 입력해주세요.',
+  })
+  password: string;
+}
