c201fa856eeab2a9df4bbcc14aad04fd04a6a678
ungheung <t01047377477@gmail.com>
2024-11-13 11:27:05 +0900

    update : cors 추가

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,20 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+  app.enableCors({
+    origin: ['http://localhost:3000', 'http://localhost:3001'],
+    credentials: true,
+  });
   await app.listen(3000);
 }
 bootstrap();
