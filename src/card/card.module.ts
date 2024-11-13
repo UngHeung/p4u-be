5516f728dd2146bf086d963aa09562b595801b92
ungheung <t01047377477@gmail.com>
2024-11-13 19:17:19 +0900

    update : 가드 사용을 위한 모듈 추가

--- src/card/card.module.ts
+++ src/card/card.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from 'src/auth/auth.module';
 import { Tag } from 'src/tag/entity/tag.entity';
 import { TagModule } from 'src/tag/tag.module';
 import { UserModule } from 'src/user/user.module';
@@ -8,7 +9,12 @@ import { CardService } from './card.service';
 import { Card } from './entity/card.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Card, Tag]), UserModule, TagModule],
+  imports: [
+    TypeOrmModule.forFeature([Card, Tag]),
+    UserModule,
+    AuthModule,
+    TagModule,
+  ],
   exports: [CardService],
   controllers: [CardController],
   providers: [CardService],
--- src/tag/tag.module.ts
+++ src/tag/tag.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from 'src/auth/auth.module';
 import { Tag } from './entity/tag.entity';
 import { TagController } from './tag.controller';
 import { TagService } from './tag.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Tag])],
+  imports: [TypeOrmModule.forFeature([Tag]), AuthModule],
   exports: [TagService],
   controllers: [TagController],
   providers: [TagService],
