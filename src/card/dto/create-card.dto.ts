606f64273a79d602ce35c1fb277f20ef7c6f5887
ungheung <t01047377477@gmail.com>
2024-11-13 20:25:47 +0900

    update : 태그 입력 방식 변경

--- src/card/dto/create-card.dto.ts
+++ src/card/dto/create-card.dto.ts
@@ -1,8 +1,9 @@
 import { PickType } from '@nestjs/mapped-types';
+import { IsArray, IsNotEmpty } from 'class-validator';
 import { Card } from '../entity/card.entity';
 
-export class CreateCardDto extends PickType(Card, [
-  'title',
-  'content',
-  'tags',
-]) {}
+export class CreateCardDto extends PickType(Card, ['title', 'content']) {
+  @IsArray()
+  @IsNotEmpty()
+  keywords: string[];
+}
