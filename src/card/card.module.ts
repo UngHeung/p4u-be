72c333c90318ad34600df24baaa102751d95ad9c
ungheung <t01047377477@gmail.com>
2024-11-13 18:48:26 +0900

    update : 모듈 의존 추가

--- src/card/card.module.ts
+++ src/card/card.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
-import { CardService } from './card.service';
-import { CardController } from './card.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Card } from './entity/card.entity';
+import { Tag } from 'src/tag/entity/tag.entity';
+import { TagModule } from 'src/tag/tag.module';
 import { UserModule } from 'src/user/user.module';
+import { CardController } from './card.controller';
+import { CardService } from './card.service';
+import { Card } from './entity/card.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Card]), UserModule],
+  imports: [TypeOrmModule.forFeature([Card, Tag]), UserModule, TagModule],
   exports: [CardService],
   controllers: [CardController],
   providers: [CardService],
