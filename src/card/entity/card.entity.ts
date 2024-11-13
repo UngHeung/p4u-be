fb33aca84de6c3b6b71c3fed0e2a32afac8be1a1
ungheung <t01047377477@gmail.com>
2024-11-13 18:47:22 +0900

    update : 주인변경, 익명여부 추가

--- src/card/entity/card.entity.ts
+++ src/card/entity/card.entity.ts
@@ -1,4 +1,4 @@
-import { IsString, Length } from 'class-validator';
+import { IsBoolean, IsString, Length } from 'class-validator';
 import { BaseModel } from 'src/common/entity/base.entity';
 import { lengthValidationMessage } from 'src/common/validation/message/length-validation.message';
 import { stringValidationMessage } from 'src/common/validation/message/type-validation.message';
@@ -18,10 +18,15 @@ export class Card extends BaseModel {
   @IsString({ message: stringValidationMessage })
   content: string;
 
+  @Column({ nullable: false, default: false })
+  @IsBoolean()
+  isAnonymity: boolean;
+
   @ManyToOne(() => User, user => user.cards)
   writer: User;
 
-  @ManyToMany(() => Tag, tag => tag.card)
+  @ManyToMany(() => Tag, tag => tag.cards)
+  @JoinColumn()
   tags: Tag[];
 
   @ManyToMany(() => User, user => user.pickCards, { onDelete: 'CASCADE' })
