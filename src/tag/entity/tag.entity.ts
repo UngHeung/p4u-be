4faa48e8db629a687c1438410b9cfecbefaa318e
ungheung <t01047377477@gmail.com>
2024-11-13 18:47:47 +0900

    update : keyword 유일성 추가, 카드 관계 종속 변경

--- src/tag/entity/tag.entity.ts
+++ src/tag/entity/tag.entity.ts
@@ -3,17 +3,16 @@ import { Card } from 'src/card/entity/card.entity';
 import { BaseModel } from 'src/common/entity/base.entity';
 import { lengthValidationMessage } from 'src/common/validation/message/length-validation.message';
 import { stringValidationMessage } from 'src/common/validation/message/type-validation.message';
-import { Column, Entity, JoinColumn, ManyToMany } from 'typeorm';
+import { Column, Entity, ManyToMany } from 'typeorm';
 
 @Entity({ name: 'tags' })
 export class Tag extends BaseModel {
-  @Column({ nullable: false })
+  @Column({ nullable: false, unique: true })
   @Length(2, 8, { message: lengthValidationMessage })
   @IsString({ message: stringValidationMessage })
   @Matches(/[a-zA-Z가-힣]/g)
   keyword: string;
 
   @ManyToMany(() => Card, card => card.tags)
-  @JoinColumn()
-  card: Card[];
+  cards: Card[];
 }
