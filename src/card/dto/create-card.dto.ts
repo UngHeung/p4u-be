12c59a3f8568e1c44ab3ef00797e7d12eddbecd1
ungheung <t01047377477@gmail.com>
2024-11-14 22:21:45 +0900

    update : 카드 생성 dto에 익명여부 추가

--- src/card/dto/create-card.dto.ts
+++ src/card/dto/create-card.dto.ts
@@ -1,9 +1,12 @@
 import { PickType } from '@nestjs/mapped-types';
-import { IsArray, IsNotEmpty } from 'class-validator';
+import { IsArray, IsBoolean, IsNotEmpty } from 'class-validator';
 import { Card } from '../entity/card.entity';
 
 export class CreateCardDto extends PickType(Card, ['title', 'content']) {
   @IsArray()
   @IsNotEmpty()
   keywords: string[];
+
+  @IsBoolean()
+  isAnonymity?: boolean;
 }
