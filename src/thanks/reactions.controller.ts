a0258fee97a8f32029d3ce1c1966079c729c6815
ungheung <t01047377477@gmail.com>
2024-11-27 03:00:10 +0900

    feat : 리액션 생성

---
+++ src/thanks/reactions.controller.ts
@@ -0,0 +1,32 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import { AccessTokenGuard } from 'src/auth/guards/bearer-token.guard';
+import { CreateReactionDto } from './dto/createReactions.dto';
+import { UpdateReactionDto } from './dto/updateReactions.dto';
+import { Reaction } from './entity/reaction.entity';
+import { ReactionsService } from './reactions.service';
+
+@Controller('thanks/reactions')
+export class ReactionsController {
+  constructor(private readonly reactionsService: ReactionsService) {}
+
+  @Post(':id')
+  @UseGuards(AccessTokenGuard)
+  async createReaction(
+    @Req() req,
+    @Body() dto: CreateReactionDto,
+    @Param('id', ParseIntPipe) id: number,
+  ): Promise<Reaction> {
+    return await this.reactionsService.createReaction(req.user, dto, id);
+  }
+}
