70388caa992f6ac5422dc49cbf5ca0d35e3cacfe
ungheung <t01047377477@gmail.com>
2024-12-01 21:56:42 +0900

    update : code, email 관련 종속 추가

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,14 +1,19 @@
 import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from 'src/auth/auth.module';
+import { EmailService } from 'src/auth/email.service';
+import { ResetCode } from 'src/auth/entity/reset-code.dto';
 import { User } from './entity/user.entity';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User]), forwardRef(() => AuthModule)],
+  imports: [
+    TypeOrmModule.forFeature([User, ResetCode]),
+    forwardRef(() => AuthModule),
+  ],
   exports: [UserService],
   controllers: [UserController],
-  providers: [UserService],
+  providers: [UserService, EmailService],
 })
 export class UserModule {}
