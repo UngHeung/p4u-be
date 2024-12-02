d0326616fd9461c01358381d4313d0e93c4793a2
ungheung <t01047377477@gmail.com>
2024-12-02 15:11:46 +0900

    update : 별명표시여부 추가

--- src/user/dto/update-user-nickname.dto.ts
+++ src/user/dto/update-user-nickname.dto.ts
@@ -1,4 +1,4 @@
-import { IsString, Length } from 'class-validator';
+import { IsBoolean, IsString, Length } from 'class-validator';
 import { lengthValidationMessage } from 'src/common/validation/message/length-validation.message';
 import { stringValidationMessage } from 'src/common/validation/message/type-validation.message';
 import { User } from '../entity/user.entity';
@@ -7,4 +7,7 @@ export class UpdateUserNicknameDto implements Partial<User> {
   @Length(2, 6, { message: lengthValidationMessage })
   @IsString({ message: stringValidationMessage })
   nickname: string;
+
+  @IsBoolean()
+  isShowNickname: boolean;
 }
